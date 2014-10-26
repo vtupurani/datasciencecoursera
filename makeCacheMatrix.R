64c96dc81e38af2da30a4e9c00599fd739ac3e7a
vtupurani <vtupurani@users.noreply.github.com>
2014-10-25 20:12:54 -0400

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,18 +1,23 @@
+##this program is to create a square matrix and with cachesolve function get the inverse of the matrix.
 makeCacheMatrix <- function(M = matrix()) { 
           i  <- NULL 
            set  <- function(Data){ 
                  M <<- Data 
                    i <<- NULL  
            } 
-           get  <- function() M 
-           setinverse  <- function(inverse) i  <<- inverse 
-           getinverse  <- function() i 
+           get  <- function(){
+           M 
+           }
+           setinverse  <- function(inverse) {
+           i  <<- inverse 
+           }
+           getinverse  <- function() {
+           i
+           }
            list(set= set, get = get,  
                 setinverse = setinverse,  
                 getinverse = getinverse) 
-   
-  
-   } 
+      } 
 
 cacheSolve <- function(M, ...) { 
          i  <- M$getinverse() 
